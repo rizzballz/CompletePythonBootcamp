078a1d807802d4683c6e72c3b3cca36c178f8771
Ryan <ryan.j.mcmillan34@gmail.com>
2020-01-01 23:12:38 +1100

    Created stacks.py

---
+++ stacks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 01 20:59:03 2020
+
+@author: Ryan McMillan
+@title: Stacks
+@description: Create a program that checks for balanced parenthesis, 
+    brackets and braces (), [], {}. Use stacks to do this 
+"""
+
+class Stack(object):
+    '''
+    Stack class to describe a Stack object. Similar to a list in that 
+    you can pop from the top (pull), append to the end of the stack 
+    (push)
+    '''
+    def __init__(self):
+        '''
+        docstring
+        '''
+        pass
+
+    def push(self, item):
+        '''
+        docstring
+        '''
+        pass
+
+    def pull(self, item):
+        '''
+        docstring
+        '''
+        pass
